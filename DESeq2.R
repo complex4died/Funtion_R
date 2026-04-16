2e40d95c662949bf16bc2bca862788705217e25b
Mr.NULL <muraiwm80wvvgipx@gmail.com>
2026-04-16 17:54:44 +0800

    Add files via upload
    This is a DESeq2 script.

---
+++ DESeq2.R
@@ -0,0 +1,39 @@
+library(DESeq2)
+library(tidyverse)
+
+
+deseq_ana <- function(matrix, 
+                      sample_info){
+  dds <- DESeqDataSetFromMatrix(
+    countData = matrix,      
+    colData = sample_info,                
+    design = ~ Group                     
+  )
+  
+  keep <- rowSums(counts(dds) >= 10) >= 3 
+  dds <- dds[keep, ]
+  
+  dds <- DESeq(dds)
+  
+  res <- data.frame(results(dds), 
+                    stringsAsFactors = FALSE, 
+                    check.names = FALSE)
+  logFC_t <- 1.5 
+  res_na <- res %>%
+    filter(!is.na(pvalue)) %>% 
+    arrange(desc(log2FoldChange)) %>% 
+    mutate(
+      change = case_when(
+        pvalue > 0.05 ~ "Stable",
+        abs(log2FoldChange) < logFC_t ~ "Stable",
+        log2FoldChange >= logFC_t ~ "Up",
+        log2FoldChange <= -logFC_t ~ "Down",
+        TRUE ~ "Stable"
+      )
+    )
+  
+  return(res_na)
+}
+
+
+
